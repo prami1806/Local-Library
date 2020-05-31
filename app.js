17480865c0357d084e9882b3a3e2bb932db4cb48
prami1806 <coolpro18062001@gmail.com>
2020-05-31 20:29:12 +0530

    Hosted application on Heroku

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://prami1806:bvnagar449@cluster0-fd3uc.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://prami1806:bvnagar449@cluster0-fd3uc.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB=process.env.MONGODB_URI||dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
